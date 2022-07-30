fc601cf5f9e1c15b1f1b334419839a9b0ff1933d
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2022-07-30 14:36:31 -0400

    Add project files.

---
+++ TestMod/HarmonyPatchTest.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TestMod
+{
+    [HarmonyPatch (typeof(PhantomBrigade.GameController), nameof(PhantomBrigade.GameController.Initialize))]
+    public static class HarmonyPatchTest
+    {
+        public static void Prefix()
+        {
+            Debug.Log("Harmony patch executed");
+            
+
+        }
+    }
+}
---
+++ TestMod/ModEntry.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using PhantomBrigade.Mods;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMod
+{
+    public class ModEntry : ModLink
+    {
+        public override void OnLoad(Harmony harmonyInstance)
+        {
+            Debug.Log($" {metadata.id} dir: {metadata.directory} path: {metadata.path}");
+        }
+    }
+}
